010e9f86626a1a85bc0c5f13b29c4c9e2f156be7
essenJean223 <jpfm199@gmail.com>
2023-06-04 15:11:12 -0600

    services section ready

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,19 +17,32 @@ export class HeroesComponent implements OnInit {
     name: 'Windostorm'
   }
 
-  heroes = HEROES;
+  public heroes: Hero[] = [];
   selectedHero?:Hero;
 
-  constructor() { }
+  constructor(
+    private heroService:HeroService,
+    private messageService:MessageService
+  ) { }
 
   //lifecycle hook: despues de crear un componente
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   //click event
   onSelect(hero:Hero){
     this.selectedHero=hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(
+      heroes=>{
+        this.heroes=heroes;
+      }
+    );
   }
 
 }
